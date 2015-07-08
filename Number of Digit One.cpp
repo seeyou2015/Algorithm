29225368b5e33f258408f75236fb5b0ee4b02b8a
seeyou2015 <mz1466@nyu.edu>
2015-07-07 22:23:13 -0400

    leetcode#233

---
+++ Number of Digit One.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countDigitOne(int n) {
+        int e=0;
+        int res=0;
+        int cur=n;
+        while(cur>0){
+            int remainder = cur%10;
+            if(remainder>1)
+                res+=(cur/10+1)*pow(10,e);
+            else if(remainder ==1)
+                res+=(cur/10)*pow(10,e) +1 +n%(int)(pow(10,e));
+            else
+                res+=(cur/10)*pow(10,e);
+            cur=cur/10;
+            e++;
+        }
+        return res;
+    }
+};
